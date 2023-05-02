318c91171c19a9c707264ea6f05537edca231390
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-05-02 23:47:02 +0300

    feat: обновил класс с условиями ТЗ 5

--- src/manager/InHistoryManager.java
+++ src/manager/InHistoryManager.java
@@ -2,35 +2,80 @@ package manager;
 
 import task.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InHistoryManager implements HistoryManager {
-
-    private List<Task> viewedTasks; // лист для хранения просмотренных задач
+    private Map<Integer, Node<Task>> receivedTasksMap;
+    private Node<Task> head;
+    private Node<Task> tail;
 
     public InHistoryManager() {
-        this.viewedTasks = new ArrayList<>();
+        receivedTasksMap = new HashMap<>();
     }
 
     @Override
-    public void addTask(Task task) {
-        if (viewedTasks.size() >= 10) { // проверяю, чтоб лист не превышал 10 элементов
-            viewedTasks.remove(0); // если больше 10, то задача удаляется
+    public void add(Task task) {
+        Node<Task> node = linkLast(task); // беру возвращаемую ноду
+        receivedTasksMap.put(task.getId(), node); // добавляю в мапу
+    }
+
+    @Override
+    public void remove(int id) {
+        for (Node<Task> node : receivedTasksMap.values()) {
+            Node<Task> lookedForNode = receivedTasksMap.get(node); // получаю ноду, которую искал по идентификатору
+            removeNode(lookedForNode); // удаляю ноду
+            receivedTasksMap.remove(lookedForNode); // удаляю ноду из мапы
         }
-            viewedTasks.add(task);
-    }
-
-    @Override
-    public void removeTask(int id) {
-        // при удалении Объекта из мапы в классе InMemoryManager
-        // нужно так же удалить его из листа просмотренных задач, потому что его больше не существует
-        viewedTasks.remove(viewedTasks.get(id));
     }
 
     @Override
     public List<Task> getHistory() {
-        return viewedTasks;
+        return getTasks();
+    }
+
+    public Node<Task> linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        // наставник сказал, что лучше значение добавить в начало
+        final Node<Task> newNode = new Node<>(task, tail, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        return newNode;
+    }
+
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>(); // пустой лист, куда буду складывать задачи для истории
+        Node<Task> currentNode = head; // голова списка становится нынешней нодой
+        while (!(currentNode == null)) {
+            tasks.add(currentNode.data); // добавляю данные о ноде в лист
+            currentNode = currentNode.next; // перехожу к следующему значению
+        }
+        return tasks;
+    }
+
+    private void removeNode(Node<Task> node) {
+        if (!(node == null)) {
+            final Node<Task> next = node.next;
+            final Node<Task> previous = node.previous;
+            node.data = null;
+
+            if (head == node && tail == node) {
+                head = null;
+                tail = null;
+            } else if (head == node && !(tail == node)) {
+                head = next;
+                head.previous = null;
+            } else if (!(head == node) && tail == node) {
+                tail = previous;
+                tail.next = null;
+            } else {
+                previous.next = next;
+                next.previous = previous;
+            }
+        }
     }
 
 }
