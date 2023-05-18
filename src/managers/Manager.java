92676bebfded1c479a5c073eb0aefbc03b36dd98
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-05-18 21:21:52 +0300

    Initial commit

---
+++ src/managers/Manager.java
@@ -0,0 +1,50 @@
+package managers;
+
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.Task;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface Manager {
+
+    void addTask(Task task);
+
+    Task getTaskById(int id);
+
+    void deleteTasks();
+
+    HashMap<Integer, Task> getTasks();
+
+    void updateTask(Task task);
+
+    void deleteTaskById(int id);
+
+    void addEpic(Epic epic);
+
+    Epic getEpicById(int id);
+
+    void deleteEpics();
+
+    HashMap<Integer, Epic> getEpics();
+
+    void updateEpic(Epic epic);
+
+    void deleteEpicById(int id);
+
+    void addSubtask(Subtask subtask);
+
+    Subtask getSubtaskById(int id);
+
+    void deleteSubtasks();
+
+    HashMap <Integer, Subtask> getSubtasks();
+
+    void updateSubtask(Subtask subtask);
+
+    void deleteSubtaskById(int id);
+
+    List<Task> getHistory();
+
+}
