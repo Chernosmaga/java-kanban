4dccecf1478bd7970638350079897cefba13c22e
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-06-02 22:09:14 +0300

    Initial commit

---
+++ src/history/HistoryManager.java
@@ -0,0 +1,15 @@
+package history;
+
+import tasks.Task;
+
+import java.util.List;
+
+public interface HistoryManager { // отдельный интерфейс для хранения необходимых методов
+
+    Task add(Task task); // добавляет Задачу в лист просмотренных задач
+
+    void remove(int id); // удаляет Задачу по идентификатору
+
+    List<Task> getHistory();
+
+}
