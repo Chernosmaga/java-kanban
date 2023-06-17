7d84348c0887d6a93166e118b46308bf78ea0c91
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-06-17 13:33:53 +0300

    Initial commit

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -4,15 +4,21 @@ import history.HistoryManager;
 import history.InHistoryManager;
 import memory.InMemoryManager;
 import memory.Manager;
+import servers.HttpTaskManager;
+import servers.KVTaskClient;
 
 public class Managers {
 
     public static Manager getDefault() {
-        return new InMemoryManager();
+        return new InMemoryManager(getDefaultHistory());
     }
 
     public static HistoryManager getDefaultHistory() {
         return new InHistoryManager();
+    }
+
+    public static Manager getDefault(String url) {
+        return new HttpTaskManager(getDefaultHistory(), url, new KVTaskClient(url));
     }
 
 }
